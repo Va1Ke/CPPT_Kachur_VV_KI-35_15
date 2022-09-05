79eb0750e6e0e5bae1c60e8b5bdfc938ba3e932b
Vladyslav Kachur <vladikkr89@gmail.com>
2022-09-05 10:26:45 +0300

    made lab2 using ragged array

--- KI-35_Kachur/lab2/src/lab2.java
+++ KI-35_Kachur/lab2/src/lab2.java
@@ -4,36 +4,33 @@ import java.util.Scanner;
 
 public class lab2{
 	
-	public static void printFigure(int n, String str) throws Exception {
-		int [][] array = new int[n][n];
+	public static void printFigure(int n) throws Exception {
+		int [][] array = new int[n][];
         File file = new File("MyFile.txt");
         PrintWriter fout = new PrintWriter(file);
 
-        if(str.length() != 1){
-            throw new Exception("Wrong input");
+
+        for (int i=0; i<n; i++)
+        {
+        	// allocate memory for the gear array
+        	// values changing acc. to requirement
+        	array[i] = new int[i+1];
+        	// inner loop to handle number spaces
+            // values changing acc. to requirement
+        	for(int j=0; j<n-i;j++) {
+            	System.out.print(" ");
+            	fout.print(" ");
+            }
+        	// inner loop to print elements of array
+            // values changing acc. to requirement
+        	for(int j=0; i>j;j++) {
+        		System.out.print(array[i][j]);
+        		fout.print(array[i][j]);
+            }
+        	System.out.println();
+        	fout.println("");
         }
-        // outer loop to handle number of rows
-        //  n in this case
-        for(int i=0; i<n;i++) {
-			for(int j=0; j<n;j++) {
-				array[i][j]=0;
-			}
-		}
         
-        //loops to handle of columns
-        for(int i=1; i<n;i++) {
-			for(int j=n-1; j>=0;j--) {
-				if(i>=j) {
-					array[i][j]=1;
-				}
-				// printing stars
-				fout.print(array[i][j]);
-				System.out.print(array[i][j]);
-			}
-			// ending line after each row
-			fout.println("");
-			System.out.println("");
-		}
         fout.flush();
         fout.close();
     }
@@ -50,9 +47,7 @@ public class lab2{
         System.out.print("Enter length-width of a square: ");
         int n = scanner.nextInt();
         scanner.nextLine();
-        System.out.print("\nEnter symbol-filler of the square matrix: ");
-        String str = scanner.nextLine();
 
-        printFigure(n, str);
+        printFigure(n);
 	}
 }
